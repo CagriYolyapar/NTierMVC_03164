662f4fd397d272bb6a4912c131481b92c23631f4
cagri.yolyapar <cagri.yolyapar@gmail.com>
2023-03-16 13:32:55 +0300

    BLL katmanı tamamlandı

---
+++ Project.BLL/RepositoryPattern/BaseRep/BaseRepository.cs
@@ -0,0 +1,150 @@
+﻿using Project.BLL.DesignPatterns.SingletonPattern;
+using Project.BLL.RepositoryPattern.IntRep;
+using Project.DAL.ContextClasses;
+using Project.ENTITIES.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.BLL.RepositoryPattern.BaseRep
+{
+    public abstract class BaseRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        protected MyContext _db;
+        public BaseRepository()
+        {
+            _db = DBTool.DBInstance;
+        }
+
+        protected void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Add(T item)
+        {
+            _db.Set<T>().Add(item);
+            Save();
+        }
+
+        public void AddRange(List<T> list)
+        {
+            _db.Set<T>().AddRange(list);
+            Save();
+        }
+
+        public bool Any(Expression<Func<T, bool>> exp)
+        {
+            return _db.Set<T>().Any(exp);
+        }
+
+        /// <summary>
+        /// Bu işlem veriyi sadece pasife ceker
+        /// </summary>
+        /// <param name="item"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        public void Delete(T item)
+        {
+            item.Status = ENTITIES.Enums.DataStatus.Deleted;
+            item.DeletedDate = DateTime.Now;
+            Save();
+        }
+
+        public void DeleteRange(List<T> list)
+        {
+            foreach (T item in list) Delete(item);
+        }
+
+        public void Destroy(T item)
+        {
+            _db.Set<T>().Remove(item);
+            Save();
+        }
+
+        public void DestroyRange(List<T> list)
+        {
+            _db.Set<T>().RemoveRange(list);
+            Save();
+        }
+
+        public T Find(int id)
+        {
+            return _db.Set<T>().Find(id);
+        }
+
+        public T FirstOrDefault(Expression<Func<T, bool>> exp)
+        {
+            return _db.Set<T>().FirstOrDefault(exp);
+        }
+
+        public List<T> GetActives()
+        {
+            return Where(x => x.Status != ENTITIES.Enums.DataStatus.Deleted);
+        }
+
+        public List<T> GetAll()
+        {
+            return _db.Set<T>().ToList();
+        }
+
+        public List<T> GetFirstDatas(int count)
+        {
+            return _db.Set<T>().OrderBy(x => x.CreatedDate).Take(count).ToList();
+        }
+
+        public List<T> GetLastDatas(int count)
+        {
+            return _db.Set<T>().OrderByDescending(x => x.CreatedDate).Take(count).ToList();
+
+        }
+
+        public List<T> GetModifieds()
+        {
+            return Where(x => x.Status == ENTITIES.Enums.DataStatus.Updated);
+        }
+
+        public List<T> GetPassives()
+        {
+            return Where(x => x.Status == ENTITIES.Enums.DataStatus.Deleted);
+
+        }
+
+        public List<T> OrderBy(Expression<Func<T, bool>> exp)
+        {
+            return _db.Set<T>().OrderBy(exp).ToList();  
+        }
+
+        public IQueryable<X> Select<X>(Expression<Func<T, X>> exp)
+        {
+            return _db.Set<T>().Select(exp); 
+        }
+
+        public object Select(Expression<Func<T, object>> exp)
+        {
+            return _db.Set<T>().Select(exp).ToList();
+        }
+
+        public void Update(T item)
+        {
+            item.ModifiedDate = DateTime.Now;
+            item.Status = ENTITIES.Enums.DataStatus.Updated;
+
+            T originalData = Find(item.ID);
+            _db.Entry(originalData).CurrentValues.SetValues(item);
+            Save();
+        }
+
+        public void UpdateRange(List<T> list)
+        {
+            foreach (T item in list) Update(item);
+        }
+
+        public List<T> Where(Expression<Func<T, bool>> exp)
+        {
+            return _db.Set<T>().Where(exp).ToList();
+        }
+    }
+}
---
+++ Project.BLL/RepositoryPattern/ConcRep/ArticleRepository.cs
@@ -0,0 +1,26 @@
+﻿using Project.BLL.RepositoryPattern.BaseRep;
+using Project.ENTITIES.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.BLL.RepositoryPattern.ConcRep
+{
+    public class ArticleRepository:BaseRepository<Article>
+    {
+        public void AddArticleWithAuthor(Article a,Author author)
+        {
+            Article newArticle = new Article
+            {
+                Title = a.Title,
+                Content = a.Content,
+                Author = author
+            };
+
+            _db.Articles.Add(newArticle);
+            Save();
+        }
+    }
+}
