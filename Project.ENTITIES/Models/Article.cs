383ba3e3f4db16c42f2945a519570c6bd6c448f9
cagri.yolyapar <cagri.yolyapar@gmail.com>
2023-03-16 11:46:48 +0300

    Add project files.

---
+++ Project.ENTITIES/Models/Article.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.ENTITIES.Models
+{
+    public class Article:BaseEntity
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int? CategoryID { get; set; }
+        public int? AuthorID { get; set; }
+
+
+        //Relational Properties
+        public virtual Category Category { get; set; }
+        public virtual Author Author { get; set; }
+
+    }
+}
---
+++ Project.ENTITIES/Models/Author.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.ENTITIES.Models
+{
+    public class Author : BaseEntity
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        //Relational Properties
+        public virtual List<Article> Articles { get; set; }
+        public virtual AuthorProfile Profile { get; set; }
+
+    }
+}
