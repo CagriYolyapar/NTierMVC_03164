1c2f01f2bd74ea64898d3fb7b8b12f2ac0754aa8
cagri.yolyapar <cagri.yolyapar@gmail.com>
2023-03-16 13:35:31 +0300

    ArticleRepository icin yeni yontemler eklendi

--- Project.BLL/RepositoryPattern/ConcRep/ArticleRepository.cs
+++ Project.BLL/RepositoryPattern/ConcRep/ArticleRepository.cs
@@ -12,15 +12,33 @@ namespace Project.BLL.RepositoryPattern.ConcRep
     {
         public void AddArticleWithAuthor(Article a,Author author)
         {
-            Article newArticle = new Article
-            {
-                Title = a.Title,
-                Content = a.Content,
-                Author = author
-            };
+            //Article newArticle = new Article
+            //{
+            //    Title = a.Title,
+            //    Content = a.Content,
+            //    Author = author
+            //};
 
-            _db.Articles.Add(newArticle);
-            Save();
+            //_db.Articles.Add(newArticle);
+            //Save();
+
+            #region IkinciYontem
+
+            //Save işleminden sonra Article'in yazarının verilebilmesi ek bir güncelleme işlemi yapılması gerekir.
+
+            //_db.Articles.Add(a);
+            //_db.Authors.Add(author);
+            //Save(); 
+            #endregion
+
+            #region UcuncuYontem
+            _db.Authors.Add(author);
+            Save(); // Author'in Save sayesinde ID'si Identity ile olusur...
+
+            a.AuthorID = author.ID;
+            _db.Articles.Add(a);
+            Save(); 
+            #endregion
         }
     }
 }
